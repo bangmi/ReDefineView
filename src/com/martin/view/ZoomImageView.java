5acd6bfdcb9dc528ba50c42c2d8cea3dd62ce83e
bangmi <e5admin@163.com>
2016-08-15 22:58:06 +0800

    移动实现，移动边界的检测

--- src/com/martin/view/ZoomImageView.java
+++ src/com/martin/view/ZoomImageView.java
@@ -1,5 +1,6 @@
 package com.martin.view;
 
+import android.R.integer;
 import android.content.Context;
 import android.graphics.Matrix;
 import android.graphics.RectF;
@@ -9,6 +10,7 @@ import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.ScaleGestureDetector.OnScaleGestureListener;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewTreeObserver.OnGlobalLayoutListener;
 import android.widget.ImageView;
 import android.view.View.OnTouchListener;
@@ -17,7 +19,6 @@ public class ZoomImageView extends ImageView implements OnGlobalLayoutListener,
 		OnScaleGestureListener, OnTouchListener {
 
 	private boolean isOnec = true;
-
 	// 初始化时应该进行的缩放
 	private float mInitScanle;
 	// 缩小的最大值
@@ -26,10 +27,12 @@ public class ZoomImageView extends ImageView implements OnGlobalLayoutListener,
 	private float mMaxScanle;
 	// 双击放大的值
 	private float mMidScanle;
-
+	// 图像变化控制的矩阵
 	private Matrix mMatrix;
 	// 多指触控操作的控制图片的放大缩小
 	private ScaleGestureDetector mScaleGestureDetector;
+
+	// ------自由移动---------
 
 	public ZoomImageView(Context context, AttributeSet attrs) {
 		this(context, attrs, 0);
@@ -45,6 +48,7 @@ public class ZoomImageView extends ImageView implements OnGlobalLayoutListener,
 		super.setScaleType(ScaleType.MATRIX);
 		mScaleGestureDetector = new ScaleGestureDetector(context, this);
 		this.setOnTouchListener(this);
+		mtouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
 	}
 
 	@Override
@@ -144,12 +148,12 @@ public class ZoomImageView extends ImageView implements OnGlobalLayoutListener,
 
 	@Override
 	public boolean onScaleBegin(ScaleGestureDetector detector) {
+		preScale = 1.0f;
 		return true;
 	}
 
 	@Override
 	public void onScaleEnd(ScaleGestureDetector detector) {
-		preScale = 1.0f;
 		if (getScale() < mInitScanle) {
 
 			mMatrix.postScale(mInitScanle / getScale(), mInitScanle / getScale(), getWidth() / 2,
@@ -159,7 +163,7 @@ public class ZoomImageView extends ImageView implements OnGlobalLayoutListener,
 	}
 
 	/**
-	 * 缩放时进行辩解控制，位置控制
+	 * 缩放时进行边界控制，防止出现不合理的显示位置
 	 */
 	private void checkBorder() {
 		RectF rectF = getMatrixRectF();
@@ -209,9 +213,79 @@ public class ZoomImageView extends ImageView implements OnGlobalLayoutListener,
 		return rectF;
 	}
 
+	int mLastPointCount = 0;
+	float mLastX;
+	float mLastY;
+	private boolean isCanMoveable;
+
 	@Override
 	public boolean onTouch(View v, MotionEvent event) {
 		mScaleGestureDetector.onTouchEvent(event);
+		float tx = 0;
+		float ty = 0;
+		// 手指的中心点位置
+		int pointCount = event.getPointerCount();
+		for (int i = 0; i < pointCount; i++) {
+			tx += event.getX(i);
+			ty += event.getY(i);
+		}
+		tx /= pointCount;
+		ty /= pointCount;
+		if (pointCount != mLastPointCount) {
+			mLastX = tx;
+			mLastY = ty;
+			mLastPointCount = pointCount;
+		}
+
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_MOVE:
+			float dx = tx - mLastX;
+			float dy = ty - mLastY;
+			if (!isCanMoveable) {
+
+				if (isMoveAction(dx, dy)) {
+					isCanMoveable = true;
+				}
+
+			} else {
+				RectF rectF = getMatrixRectF();
+				if (rectF.width() < getWidth()) {
+					dx = 0;
+				}
+				if (rectF.height() < getHeight()) {
+					dy = 0;
+				}
+
+				mMatrix.postTranslate(dx, dy);
+				//移动的边界检测
+				RectF moveRectF = getMatrixRectF();
+				if (moveRectF.left > 0 || moveRectF.right < getWidth()) {
+					mMatrix.postTranslate(-dx, 0);
+				}
+				if (moveRectF.top > 0 || moveRectF.bottom < getHeight()) {
+					mMatrix.postTranslate(0, -dy);
+				}
+				setImageMatrix(mMatrix);
+			}
+			mLastX = tx;
+			mLastY = ty;
+			break;
+		case MotionEvent.ACTION_UP:
+		case MotionEvent.ACTION_CANCEL:
+			mLastPointCount = 0;
+			break;
+
+		default:
+			break;
+		}
+
 		return true;
+	}
+
+	int mtouchSlop;
+
+	private boolean isMoveAction(float dx, float dy) {
+
+		return Math.sqrt(dx * dx + dy * dy) > mtouchSlop;
 	}
 }
